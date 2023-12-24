14d47658d544a9eccc0246780d1dde4663db7c10
CarterTsai <Carter.Tsai@ibm.com>
2023-12-24 22:39:43 +0800

    add spark mongo test

--- airflow-dags/dags/test/spark/spark_mongo_test.py
+++ airflow-dags/dags/test/spark/spark_mongo_test.py
@@ -3,29 +3,30 @@ from pyspark.sql.functions import col, struct, explode, lit, collect_list, max, 
 from pyspark.sql.window import Window
 import os
 
-
-# package file path
-# java_path = '/home/jovyan/work/packages/mongo-jar/mongo-java-driver-3.12.14.jar'
-# jar_path = '/home/jovyan/work/packages/mongo-jar/mongo-spark-connector_2.12-2.4.0.jar'
-java_path = '/usr/local/bin/mongo-java-driver-3.12.14.jar'
-jar_path = '/usr/local/bin/mongo-spark-connector_2.12-2.4.0.jar'
-
-mongodb_uri = "mongodb://root:172.27.48.1:27017/?authSource=root" 
+mongodb_uri = "mongodb://test:test@172.27.48.1:27017/test.cart" 
 
 
 spark = SparkSession \
     .builder \
     .appName("myApp") \
-    .config('spark.jars', jar_path)\
-    .config('spark.driver.extraClassPath', java_path)\
     .config("spark.mongodb.input.uri", mongodb_uri) \
     .config("spark.mongodb.output.uri", mongodb_uri) \
     .getOrCreate()
 
+
+spark = SparkSession \
+    .builder \
+    .appName("myApp") \
+    .config("spark.mongodb.input.uri", mongodb_uri) \
+    .config("spark.mongodb.output.uri", mongodb_uri) \
+    .config('spark.driver.extraClassPath', '/opt/spark/jars/*') \
+    .getOrCreate()
+
 df = spark.read \
-    .format("com.mongodb.spark.sql.DefaultSource") \
-    .option("spark.mongodb.input.database", "test") \
-    .option("spark.mongodb.input.collection", "cart") \
+    .format("mongodb") \
+    .option("spark.mongodb.read.database", "test") \
+    .option("spark.mongodb.read.collection", "cart") \
+    .option("spark.mongodb.read.connection.uri", "mongodb://test:test@172.27.48.1:27017/test.cart") \
     .load()
 
 df.show()
